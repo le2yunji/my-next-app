9e58b4ee6d9fb501be59abfe653d387ccd163f9a
le2yunji <le2yunji@naver.com>
2026-03-10 16:37:40 +0900

    Feat: 전역 로딩 화면

--- my-next-app/app/loading.tsx
+++ my-next-app/app/loading.tsx
@@ -1,5 +1,12 @@
 // app/loading.tsx
+import Spinner from "@/components/common/Spinner";
 
 export default function Loading() {
-  return <main className="mx-auto max-w-xl p-4 bg-blue-600"></main>;
+  return (
+    <main className="mx-auto flex min-h-[60vh] max-w-xl items-center justify-center p-4">
+      <div className="flex flex-col items-center gap-3 text-gray-500">
+        <Spinner size={50} className="text-primary-500" />
+      </div>
+    </main>
+  );
 }
